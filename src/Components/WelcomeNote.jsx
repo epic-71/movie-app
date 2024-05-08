eed561a83e327c221d1c59c7a9b6090d5da24c35
epic-71 <teamepic71@gmail.com>
2024-05-08 16:30:27 +0530

    created welcome note component

---
+++ src/Components/WelcomeNote.jsx
@@ -0,0 +1,9 @@
+function WelcomeNote() {
+  return (
+    <div className="bg-primary_black text-white w-full h-full flex flex-col items-center pt-48">
+      <h1 className="text-3xl font-bold">Welcome to the <span className="text-primary_yellow">MOVIE APP</span></h1>
+    </div>
+  );
+}
+
+export default WelcomeNote;
